c7850021c786e81afce4cec71d503d7cd584df5e
rameyer <meyer@mpia.de>
2021-10-04 14:00:21 +0200

    reformat setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,8 @@
 '''
 interferopy
-
 Setup script
 '''
-# prefer setuptools over distutils, but fall back if not present
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+from setuptools import setup
+setup(use_scm_version=True)
 
-with open('requirements.txt', "r") as f:
-    requirements = f.readlines()
-
-
-setup(name='interferopy',
-      version='0.5',
-      author='Leindert Boogaard, Romain Meyer, Mladen Novak',
-      author_email='novak@mpia.de',
-      description='python library for common tasks in radio/submm interferometric data analysis',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/mladenovak/interferopy",
-      project_urls={
-          "Bug Tracker": "https://github.com/mladenovak/interferopy/issues"
-      },
-      python_requires=">=3.7",
-      install_requires=requirements,
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-      ],
-      packages=['interferopy']
-      )
