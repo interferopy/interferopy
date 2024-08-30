259387a4bc338afc80bfc1844ad4cb457df505c6
Leindert Boogaard <leindertboogaard@gmail.com>
2024-08-30 11:17:01 +0200

    bump conf

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../interferopy'))
 # -- Project information -----------------------------------------------------
 
 project = 'Interferopy'
-copyright = '2021, Leindert Boogaard, Romain Meyer, Mladen Novak'
+copyright = '2024, Leindert Boogaard, Romain Meyer, Mladen Novak'
 author = 'Leindert Boogaard, Romain Meyer, Mladen Novak'
 
 # The full version, including alpha/beta/rc tags
